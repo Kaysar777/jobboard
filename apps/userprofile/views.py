d1961f87dd5bd5d17682c3ef641ba8cb6fc67623
Kaisar Butin <bolawak@gmail.com>
2021-03-21 23:32:31 +0600

    commit message

---
+++ apps/userprofile/views.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import ConversationMessage
+
+from apps.notification.utilities import create_notification
+from apps.job.models import Application, Job
+
+# Create your views here.
+@login_required
+def dashboard(request):
+    return render(request, 'userprofile/dashboard.html', {'userprofile':request.user.userprofile})
+
+@login_required
+def view_application(request, application_id):
+    if request.user.userprofile.is_employer:
+        application = get_object_or_404(Application, pk=application_id, job__created_by=request.user)
+    else:
+        application = get_object_or_404(Application, pk=application_id, created_by=request.user)
+    
+    if request.method == 'POST':
+        content = request.POST.get('content')
+
+        if content:
+            conversationmessage = ConversationMessage.objects.create(application=application, content=content, created_by=request.user)
+            if request.user.userprofile.is_employer:
+                to_user = application.created_by
+            else:
+                to_user = application.job.created_by
+            create_notification(request, to_user, 'message', extra_id=application.id)
+            
+            print('Message posted: ' + content)
+            return redirect('view_application', application_id=application_id)
+
+    return render(request, 'userprofile/view_application.html', {'application':application})
+
+@login_required
+def view_dashboard_job(request, job_id):
+    job = get_object_or_404(Job, pk=job_id, created_by=request.user)
+
+    return render(request, 'userprofile/view_dashboard_job.html', {'job':job})
